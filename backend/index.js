abce60391231e1e72d86584a292ba361c45243eb
shripad_69 <shripadalgude@gmail.com>
2025-12-18 21:00:49 +0530

    added health check

--- backend/index.js
+++ backend/index.js
@@ -153,6 +153,11 @@ app.get("/cicd-check", (req, res) => {
 });
 
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
